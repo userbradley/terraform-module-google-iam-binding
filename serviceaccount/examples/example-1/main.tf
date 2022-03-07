ae99f9afcc855f3955665cbc1b0f435f292a7e4c
Bradley <bradley@breadnet.co.uk>
2022-03-07 13:20:01 +0000

    updated example

--- serviceaccount/examples/example-1/main.tf
+++ serviceaccount/examples/example-1/main.tf
@@ -1,5 +1,5 @@
 module "iam-binding" {
-  source = "git::ssh://git@github.com/userbradley/terraform-module-google-iam-binding.git//serviceaccount"
+  source = "git::ssh://git@github.com/userbradley/terraform-module-google-iam-binding.git//serviceaccount?ref=6fe23ee523c0effe8282a4201a2eb01dffee761c"
   email  = google_service_account.cloudrun.email
   project = var.project
   role = google_project_iam_custom_role.cloudrun.role_id
